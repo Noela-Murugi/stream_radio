1e98e929437201c61c4cb49e932b1c52f37db671
Noela-Murugi <murugin254@gmail.com>
2022-08-12 19:04:52 +0300

    add event listeners to scripts.js file

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const player = document.querySelector('.audio-player');
 const controlButton = document.querySelector('.playBtn');
 const currentChannel = document.querySelector('.current-channel');
 const popupMessage = document.querySelector('.popup');
+
+
+//Event Listeners
+  document.addEventListener('DOMContentLoaded', ()=>{
+    let listHTML = '';
+
+    stationsData.forEach(listItem =>{
+      listHTML += `
+      <div class="station-item" id="${listItem.id}">
+        <img src="${listItem.logo}">
+        <label>${listItem.name}</label>
+      </div>
+      `;
+    });
+
+    stationsList.innerHTML = listHTML;
+
+    console.log(controlButton);
+
+  });
